25719c54864c795bda90590addd761c56dc4e106
Sergey <bot74@mail.ru>
2017-01-24 22:53:55 +0200

    lesson13

---
+++ lesson13_AsyncTask/app/src/main/java/com/example/sergey/lesson13_asynctask/MainActivity.java
@@ -0,0 +1,121 @@
+package com.example.sergey.lesson13_asynctask;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnShowProgress;
+    private ImageView my_image;
+    private static File imageRoot;
+    private ProgressDialog pDialog;
+
+    public static final int progress_bar_type = 0;
+    private static String file_url = "http://pulson.ru/wp-content/uploads/2013/03/pulson362.jpg";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnShowProgress = (Button) findViewById(R.id.button);
+        my_image = (ImageView) findViewById(R.id.imageView);
+
+        btnShowProgress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // starting new Async Task
+                Log.d("Tag", "onClick : ");
+                new MyTask().execute(file_url);
+            }
+        });
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case progress_bar_type:
+                pDialog = new ProgressDialog(this);
+                pDialog.setMessage("Downloading file. Please wait...");
+                pDialog.setIndeterminate(false);
+                pDialog.setMax(100);
+                pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                pDialog.setCancelable(true);
+                pDialog.show();
+                return pDialog;
+            default:
+                return null;
+        }
+    }
+
+    private class MyTask extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showDialog(progress_bar_type);
+        }
+
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            try {
+                URL url = new URL(f_url[0]);
+                URLConnection conection = url.openConnection();
+                conection.connect();
+                int lenghtOfFile = conection.getContentLength();
+                InputStream input = new BufferedInputStream(url.openStream(), 8192);
+                imageRoot = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "image1.jpg");
+
+                OutputStream output = new FileOutputStream(imageRoot);
+                byte data[] = new byte[1024];
+                long total = 0;
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                          publishProgress((int) ((total * 100) / lenghtOfFile));
+                    Log.d("Tag", "publishProgress " + (int) ((total * 100) / lenghtOfFile));
+                    output.write(data, 0, count);
+                }
+                output.flush();
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+
+            return null;
+        }
+
+
+        protected void onProgressUpdate(Integer... progress) {
+            pDialog.setProgress(progress[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String file_url) {
+            dismissDialog(progress_bar_type);
+            String imagePath = imageRoot.getAbsolutePath();
+            Log.d("Tag", "onPostExecute : " + imagePath);
+            my_image.setImageDrawable(Drawable.createFromPath(imagePath));
+        }
+
+    }
+}
