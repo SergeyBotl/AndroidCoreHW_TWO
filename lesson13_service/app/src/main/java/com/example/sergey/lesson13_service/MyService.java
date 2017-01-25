dd5b73e731626b2e72728b3c3b9863a07ab1adf6
Sergey <bot74@mail.ru>
2017-01-25 16:55:11 +0200

    lesson13_service

---
+++ lesson13_service/app/src/main/java/com/example/sergey/lesson13_service/MyService.java
@@ -0,0 +1,70 @@
+package com.example.sergey.lesson13_service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyService extends Service {
+    private final String TAG = "tag";
+    private Handler handler;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "MyService onCreate");
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                Toast.makeText(getApplicationContext(), "Получено сообщение № " + msg.what, Toast.LENGTH_SHORT).show();
+            }
+        };
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "MyService onStartCommand  startId: " + startId);
+        someTask();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "MyService onDestroy");
+
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    void someTask() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 1; i <= 5; i++) {
+                    Log.d(TAG, "i = " + i);
+                    try {
+                        handler.sendEmptyMessage(i);
+                        TimeUnit.SECONDS.sleep(15);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                stopSelf();
+            }
+        }).start();
+
+    }
+}
