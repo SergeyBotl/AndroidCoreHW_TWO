1875786f6966c2204a2f3779baa78ca9776dc53f
Sergey <bot74@mail.ru>
2017-01-25 11:12:22 +0200

    lesson13

--- lesson13_AsyncTask/app/src/main/java/com/example/sergey/lesson13_asynctask/SecondActivity.java
+++ lesson13_AsyncTask/app/src/main/java/com/example/sergey/lesson13_asynctask/SecondActivity.java
@@ -1,63 +1,79 @@
 package com.example.sergey.lesson13_asynctask;
 
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.concurrent.TimeUnit;
 
 public class SecondActivity extends AppCompatActivity {
-    private TextView textView;
-    private final Handler handler = new Handler();
-
-    public SecondActivity() {
-    }
+    private TextView textStart, textTest;
+    private Handler handler;
+    private Button buttonStart, buttonTest;
+    private int testCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        textView = (TextView) findViewById(R.id.textView2);
+        textStart = (TextView) findViewById(R.id.textViewStart);
+        textTest = (TextView) findViewById(R.id.textViewTest);
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonTest = (Button) findViewById(R.id.buttonTest);
 
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                textStart.setText("Закачано файлов" + msg.what);
+                if (msg.what == 30) {
+                    buttonStart.setEnabled(true);
+                    Toast.makeText(SecondActivity.this, "Все закачано", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        };
     }
 
     public void onClick(View view) {
-        Thread thread=new Thread();
-                handler.post(new Runnable() {
+        switch (view.getId()) {
+            case R.id.buttonStart:
+                buttonStart.setEnabled(false);
+                Thread thread = new Thread(new Runnable() {
                     @Override
                     public void run() {
                         for (int i = 1; i <= 30; i++) {
-                            try {
-                                TimeUnit.SECONDS.sleep(2);
-                                textView.setText(i+"");
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-
+                            downloadFile();
+                            handler.sendEmptyMessage(i);
+                        }
                     }
-
-            }
-        });
-thread.start();
-    }
-
-
-
-    private class MyAsyncTask extends AsyncTask<String, Integer, Void> {
-
-        @Override
-        protected Void doInBackground(String... strings) {
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
+                });
+                thread.start();
+                break;
+            case R.id.buttonTest:
+                Log.d("TAG", "test");
+                textTest.setText("count: " + testCount++);
+                break;
+            default:
+                break;
         }
 
     }
+
+    private void downloadFile() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
